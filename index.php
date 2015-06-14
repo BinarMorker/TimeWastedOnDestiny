395a9fa41a44aff45dbd32e829e68dfbad22ed2a
François Allard <binarmorker@gmail.com>
2015-06-14 19:48:57 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <title>Time wasted on Destiny</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    
+
+    <!-- JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ request.php
@@ -0,0 +1,120 @@
+<?php
+header("Content-Type: application/json");
+
+$input_name = $_GET['user'];
+$input_console = $_GET['console'];
+
+try {
+    $response = array();
+    $account = new DestinyAccount($input_name, $input_console);
+    $account->lookup();
+    $account->get_accounts();
+    $response["displayName"] = $account->display_name;
+    if (array_key_exists(1, $account->accounts)) {
+        $account->fetch(1);
+        $xbl_time = $account->accounts[1];
+        $response["xbox"] = $xbl_time;
+    }
+    if (array_key_exists(2, $account->accounts)) {
+        $account->fetch(2);
+        $psn_time = $account->accounts[2];
+        $response["playstation"] = $psn_time;
+    }
+    $response["totalTime"] = $account->total_time;
+    echo json_encode(array("Response" => $response, "Error" => $account->error));
+    die;
+} catch (Exception $e) {
+    echo json_encode(array("Response" => "", "Error" => $account->error));
+    die;
+}
+
+class DestinyAccount {
+    
+    public $name = "";
+    public $display_name = "";
+    public $console = 0;
+    public $accounts = array();
+    public $temp_account_id = "";
+    public $total_time = 0;
+    public $error = array();
+    
+    function __construct($name, $console) {
+        $this->name = $name;
+        $this->console = $console;
+    }
+
+    function lookup($retry = false) {
+        $url = "https://www.bungie.net/platform/destiny/SearchDestinyPlayer/" . $this->console . "/" . $this->name;
+        $lookup = file_get_contents($url);
+        $response = json_decode($lookup);
+        if (!empty($response->Response)) {
+            if (!$retry) {
+                $this->error = Error::show(Error::SUCCESS, "Player found");
+            } else {
+                $this->error = Error::show(Error::WARNING, "Account found on another platform");
+            }
+            $this->temp_account_id = $response->Response[0]->membershipId;
+        } else {
+            if (!$retry) {
+                $this->swap_console();
+                $this->lookup(true);
+            } else {
+                $this->error = Error::show(Error::ERROR, "Account not found");
+                throw(new Exception());
+            }
+        }
+    }
+    
+    function swap_console() {
+        if ($this->console == 1) {
+            $this->console = 2;
+        } else if ($this->console == 2) {
+            $this->console = 1;
+        }
+    }
+
+    function add_account($console, $contents) {
+        $this->accounts[$console] = $contents;
+    }
+    
+    function add_time($time) {
+        $this->total_time += $time;
+    }
+    
+    function get_accounts() {
+        $url = "https://www.bungie.net/platform/user/GetBungieAccount/" . $this->temp_account_id . "/" . $this->console;
+        $lookup = file_get_contents($url);
+        $response = json_decode($lookup);
+        if (isset($response->Response->bungieNetUser)) {
+            $this->display_name = $response->Response->bungieNetUser->displayName;
+            foreach ($response->Response->destinyAccounts as $account) {
+                if ($account->userInfo->membershipType != $this->console && count($response->Response->destinyAccounts) == 1) {
+                    $this->error = Error::show(Error::WARNING, "Account found but played an earlier version of the game");
+                }
+                $this->add_account($account->userInfo->membershipType, json_decode(json_encode($account->userInfo), true));
+            }
+        }
+    }
+    
+    function fetch($console) {
+        $url = "https://www.bungie.net/Platform/Destiny/Stats/Account/" . $this->accounts[$console]['membershipType'] . "/" . $this->accounts[$console]['membershipId'];
+        $lookup = file_get_contents($url);
+        $response = json_decode($lookup);
+        $time_played = $response->Response->mergedAllCharacters->merged->allTime->secondsPlayed->basic->value;
+        $this->accounts[$console]['timePlayed'] = $time_played;
+        $this->add_time($time_played);
+    }
+
+}
+
+class Error {
+    
+    const SUCCESS = "Success";
+    const WARNING = "Warning";
+    const ERROR = "Error";
+    
+    static function show($error_type, $message) {
+        return array($error_type => $message);
+    }
+    
+}
