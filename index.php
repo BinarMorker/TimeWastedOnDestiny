0fedfc9e1b8da712679b33ac0353ef8b29554a0b
François Allard <binarmorker@gmail.com>
2015-06-23 11:58:28 -0400

    Added style and scripts

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
     <!-- CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="css/bootstrap-switch.min.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,10 +25,111 @@
 </head>
 
 <body>
-    
-
+    <div class="container vert-middle">
+        <div class="panel panel-default text-center">
+            <div class="panel-body">
+                <h1>Time Wasted on Destiny</h1>
+                <form role="form" id="search">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <input id="console" type="checkbox" name="console" data-on-text="Xbox" data-on-color="success" data-off-text="Playstation" data-off-color="primary" data-size="large">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="input-group">
+                                <input id="username" type="text" class="form-control" name="username" placeholder="Username">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="submit">Go!</button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div id="error"></div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-fixed panel-primary text-center">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Playstation</h3>
+                    </div>
+                    <div class="panel-body">
+                        <img class="icon" id="psn_icon" />
+                        <h4 class="loading" id="psn_name"></h4>
+                        <span id="psn_time"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-fixed panel-info text-center">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Total time spent</h3>
+                    </div>
+                    <div class="panel-body">
+                        <h4 class="loading" id="display_name"></h4>
+                        <span id="total_time"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-fixed panel-success text-center">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Xbox</h3>
+                    </div>
+                    <div class="panel-body">
+                        <img class="icon" id="xbl_icon" />
+                        <h4 class="loading" id="xbl_name"></h4>
+                        <span id="xbl_time"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-6332373031553935"
+                             data-ad-slot="7124654099"
+                             data-ad-format="auto"></ins>
+                        <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-6332373031553935"
+                             data-ad-slot="7124654099"
+                             data-ad-format="auto"></ins>
+                        <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-switch.min.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-52347626-4', 'auto');
+        ga('send', 'pageview');
+    </script>
 </body>
+
 </html>
---
+++ js/main.js
@@ -0,0 +1,101 @@
+$("[name='console']").bootstrapSwitch();
+$("document").ready(function () {
+    var platform = false;
+    $("input[name='console']").on("switchChange.bootstrapSwitch", function (event, state) {
+        platform = state;
+    });
+    $("#search").submit(function(event) {
+        event.preventDefault();
+        resetFields();
+        
+        $(".loading").attr("load", 0);
+        $(".loading").text("Loading");
+        var loading = window.setInterval(function () {
+            var dots = "";
+            var dotNum = $(".loading").attr("load");
+            dotNum++;
+            dotNum %= 4;
+            for (var i = 0; i < dotNum; i++) {
+                dots += ".";
+            }
+            $(".loading").attr("load", dotNum);
+            $(".loading").text("Loading" + dots);
+        }, 1000);
+        
+        var user = $("#username").val();
+        if (platform) {
+            var plat = 1;
+        } else {
+            var plat = 2;
+        }
+        console.log(user);
+        var jsonUrl = "request.php?console="+plat+"&user="+user;
+        $.getJSON(jsonUrl, function (json) {
+            clearInterval(loading);
+            console.log(json);
+            var success = true;
+            if ('Error' in json.Error) {
+                showError(json.Error);
+                success = false;
+            } else if ('Warning' in json.Error) {
+                showError(json.Error);
+            }
+            if (success) {
+                $("#display_name").text(json.Response.displayName);
+                $("#total_time").text(getTime(json.Response.totalTime));
+                if ('playstation' in json.Response) {
+                    $("#psn_name").text(json.Response.playstation.displayName);
+                    $("#psn_icon").attr("src", "https://www.bungie.net" + json.Response.playstation.iconPath);
+                    $("#psn_time").text(getTime(json.Response.playstation.timePlayed));
+                } else {
+                    $("#psn_name").text("Never played");
+                    $("#psn_icon").attr("src", "");
+                    $("#psn_time").text("");
+                }
+                if ('xbox' in json.Response) {
+                    $("#xbl_name").text(json.Response.xbox.displayName);
+                    $("#xbl_icon").attr("src", "https://www.bungie.net" + json.Response.playstation.iconPath);
+                    $("#xbl_time").text(getTime(json.Response.xbox.timePlayed));
+                } else {
+                    $("#xbl_name").text("Never played");
+                    $("#xbl_icon").attr("src", "");
+                    $("#xbl_time").text("");
+                }
+            } else {
+                resetFields();
+            }
+        });
+    });
+});
+
+function resetFields() {
+    $("#display_name").text("");
+    $("#total_time").text("");
+    $("#psn_name").text("");
+    $("#psn_icon").attr("src", "");
+    $("#psn_time").text("");
+    $("#xbl_name").text("");
+    $("#xbl_icon").attr("src", "");
+    $("#xbl_time").text("");
+}
+
+function showError(json) {
+    if ('Error' in json) {
+        var weight = "danger";
+        $("#error").html("<div class='panel panel-"+weight+"'><div class='panel-body'><blockquote>"+json.Error+"</blockquote></div></div>");
+    } else if ('Warning' in json) {
+        var weight = "warning";
+        $("#error").html("<div class='panel panel-"+weight+"'><div class='panel-body'><blockquote>"+json.Warning+"</blockquote></div></div>");
+    } else {
+        var weight = "success";
+        $("#error").text();
+    }
+}
+
+function getTime(seconds) {
+    var days = Math.floor(seconds / (24 * 60 * 60));
+    var hours = Math.floor(seconds / (60 * 60)) % 24;
+    var minutes = Math.floor(seconds / 60) % 60;
+    var seconds = seconds % 60;
+    return days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+}
