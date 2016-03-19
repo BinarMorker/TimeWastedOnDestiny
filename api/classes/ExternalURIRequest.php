9c713b2414f13f501b9ac2eca66fda2f341a3178
François Allard <binarmorker@gmail.com>
2016-03-19 16:54:42 -0400

    Trying to debug

--- api/classes/ExternalURIRequest.php
+++ api/classes/ExternalURIRequest.php
@@ -57,7 +57,7 @@ class ExternalURIRequest {
 		// This line is made error-free for proper error handling afterwards
 		$content = @file_get_contents($this->uri, false, $context);
 		
-		if ($content === false) {
+		if (!$content) {
 			throw new ExternalURIRequestException($this->uri, $context);
 		}
 		
