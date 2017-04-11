b4f38f25d476e1a7b73473209d812f2961d6300a
Remy Bach <remy@bach.me.uk>
2017-04-11 23:16:02 +0100

    Enable cors header for API

--- api/index.php
+++ api/index.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+
 // Autoloading of all needed classes
 spl_autoload_register(function ($class_name) {
 	include $_SERVER['DOCUMENT_ROOT'].'/api/classes/' . $class_name . '.php';
